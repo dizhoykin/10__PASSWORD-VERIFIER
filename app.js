fc8050b9909a1721630cf31407634c9ba9dd7f78
dizhoykin <dizhoykin@gmail.com>
2022-01-03 23:03:50 +0700

    Done

---
+++ app.js
@@ -0,0 +1,35 @@
+const inputs = document.querySelectorAll('input');
+const verificationButton = document.querySelector('button');
+let code = [];
+
+for (let i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('input', () => {
+    inputs[i].blur();
+    if (inputs[i] != inputs[inputs.length - 1]) {
+      inputs[i+1].focus();
+    };
+    code += inputs[i].value;
+  });
+};
+
+document.addEventListener('paste', (evt) => {
+  if (evt.target.type === 'text') {
+    let data = evt.clipboardData.getData('Text');
+    code = data;
+    data = data.split('');
+    [].forEach.call(document.querySelectorAll('input'), (node, index) => {
+        node.value = data[index];
+    });
+  }
+});
+
+verificationButton.addEventListener('click', () => {
+  code = code.substring(0, 4);
+  if (code == 1234) {
+    alert('Code verified!');
+  }
+  else {
+    alert('Code denied!');
+    alert(code);
+  }
+});
